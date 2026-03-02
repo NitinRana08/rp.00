412ce8227075e00b5ff8f179cadc65a736afe762
NitinRana08 <nitinr8229@gmail.com>
2026-03-02 21:23:10 +0530

    .
    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ function App() {
 
   return (
     <>
-      <h1 className="bg-red-600 font-bold text-white text-9xl items-center flex justify-center h-screen">App</h1>
+      
 
     </>
   )
 }
 
 export default App
+
